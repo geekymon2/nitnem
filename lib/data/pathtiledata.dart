07f57cf8e083ab030a80e67f224a250d9ff6134c
Debuggd Apps Developer <debuggdapps@gmail.com>
2020-12-13 19:47:23 +1100

    added sohila sahib path

--- lib/data/pathtiledata.dart
+++ lib/data/pathtiledata.dart
@@ -7,9 +7,12 @@ class PathTileData {
     new PathTile(3, 'Chaupai Sahib', 'cOpeI swihb', 'chaupai_sahib'),
     new PathTile(4, 'Anand Sahib', 'Anµdu swihb', 'anand_sahib'),
     new PathTile(5, 'Rehras Sahib', 'rhrwis swihb', 'rehraas_sahib'),
-    new PathTile(6, 'Tav-Prasad Savaiye', 'qÍ pRswid sv`X', 'tavprasad_savaiye'),
+    new PathTile(
+        6, 'Tav-Prasad Savaiye', 'qÍ pRswid sv`X', 'tavprasad_savaiye'),
     new PathTile(7, 'Ardas', 'Ardws', 'ardas'),
     new PathTile(8, 'Sukhmani Sahib', 'suKmnI swihb', 'sukhmani_sahib'),
-    new PathTile(9, 'Dukh Bhanjani Sahib', 'duK BMjnI swihb', 'dukh_bhanjani_sahib'),
+    new PathTile(
+        9, 'Dukh Bhanjani Sahib', 'duK BMjnI swihb', 'dukh_bhanjani_sahib'),
+    new PathTile(10, 'Sohila Sahib', 'soihlw swihb', 'sohila_sahib'),
   ];
 }
