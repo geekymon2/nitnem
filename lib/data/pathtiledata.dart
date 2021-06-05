053f1960799c0aef866a9e0f7395397b2bb89f61
Debuggd Apps Developer <debuggdapps@gmail.com>
2021-06-05 20:39:37 +1000

    aarta aarta

--- lib/data/pathtiledata.dart
+++ lib/data/pathtiledata.dart
@@ -14,5 +14,6 @@ class PathTileData {
     new PathTile(
         9, 'Dukh Bhanjani Sahib', 'duK BMjnI swihb', 'dukh_bhanjani_sahib'),
     new PathTile(10, 'Sohila Sahib', 'soihlw swihb', 'sohila_sahib'),
+    new PathTile(11, 'Aarti-Aarta', 'AwrqI-Awrqw', 'aarti_aarta'),
   ];
 }
