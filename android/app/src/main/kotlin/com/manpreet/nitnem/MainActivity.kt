b23343d25701d917e63e9465d498d90bedfdff69
Geeky Mon2 <geekymon2@gmail.com>
2025-03-21 21:29:37 +1100

    Updated gradle, and other dependencies

--- android/app/src/main/kotlin/com/manpreet/nitnem/MainActivity.kt
+++ android/app/src/main/kotlin/com/manpreet/nitnem/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.manpreet.nitnem
 
 import android.os.Bundle
-import io.flutter.app.FlutterActivity
+import io.flutter.embedding.android.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
