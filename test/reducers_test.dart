b1e5be87065bc28f9e586c06fd37535ec579411d
Geeky Mon2 <geekymon2@gmail.com>
2025-08-03 20:15:36 +1000

    Added more tests

---
+++ test/reducers_test.dart
@@ -0,0 +1,29 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:nitnem/state/appstate.dart';
+import 'package:nitnem/redux/reducers/app_state_reducer.dart';
+import 'package:nitnem/redux/actions/actions.dart';
+
+void main() {
+  group('AppState Reducer', () {
+    test('ChangeThemeAction updates themeName', () {
+      final initialState = AppState.initial();
+      final action = ChangeThemeAction('Dark');
+      final newState = appReducer(initialState, action);
+      expect(newState.options.themeName, 'Dark');
+    });
+
+    test('ToggleBoldAction updates bold', () {
+      final initialState = AppState.initial();
+      final action = ToggleBoldAction(true);
+      final newState = appReducer(initialState, action);
+      expect(newState.options.bold, true);
+    });
+
+    test('ToggleStatusAction updates showStatus', () {
+      final initialState = AppState.initial();
+      final action = ToggleStatusAction(true);
+      final newState = appReducer(initialState, action);
+      expect(newState.options.showStatus, true);
+    });
+  });
+}
---
+++ test/selectors_test.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:nitnem/state/appstate.dart';
+import 'package:nitnem/redux/selectors/selectors.dart';
+
+void main() {
+  group('Selectors', () {
+    test('themeSelector returns themeName', () {
+      final state = AppState.initial().copyWith(
+        options: AppState.initial().options.copyWith(themeName: 'Light'),
+      );
+      expect(themeSelector(state), 'Light');
+    });
+
+    test('isBoldSelector returns bold', () {
+      final state = AppState.initial().copyWith(
+        options: AppState.initial().options.copyWith(bold: true),
+      );
+      expect(isBoldSelector(state), true);
+    });
+
+    test('showStatusSelector returns showStatus', () {
+      final state = AppState.initial().copyWith(
+        options: AppState.initial().options.copyWith(showStatus: true),
+      );
+      expect(showStatusSelector(state), true);
+    });
+  });
+}
